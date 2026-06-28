9426aa0dd1af9f8549ba845a23b7583027116cd5
mian196 <mianmuzammal196@gmail.com>
2026-06-28 22:52:17 +0500

    feat: add Settings.cs class file to tracking

---
+++ Settings.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Flow.Launcher.Plugin.AppUpgrader
+{
+    public class Settings
+    {
+        public bool EnableUpgradeAll { get; set; } = false;
+        public List<string> ExcludedApps { get; set; } = new List<string>();
+    }
+}
